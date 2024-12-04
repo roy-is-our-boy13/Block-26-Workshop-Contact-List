7405a0ae0baeea8446eafa48d060ad28b190d8c0
Roy <rjperlman@gmail.com>
2024-12-04 09:15:04 -0800

    Some new files, and new code..

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ContactList from "./components/ContactList";
 
-function App() {
-  
 
-  
+export default function App() 
+{
+  return (
+    <>
+      <ContactList />
+    </>
+  );
 }
-
-export default App
